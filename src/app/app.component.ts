99ce099f73d73d6cce55e5523a6e52e0c49a8c37
XavierCortes98 <86173666+XavierCortes98@users.noreply.github.com>
2025-01-27 19:09:16 +0100

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,118 @@
 import { Component } from '@angular/core';
+import { Cell } from './models/cell.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  MAX_GUESSES = 5;
   title = 'wordle';
+  wordSolution = 'Arroz';
+  errorMsg = '';
+
+  attemptIndex = 0;
+  guessColumn = 0;
+  wordCount = 5;
+  guesses: Cell[][] = [];
+
+  constructor() {
+    this.initMatrix();
+  }
+
+  initMatrix() {
+    for (let i = 0; i < this.MAX_GUESSES; i++) {
+      const row: Cell[] = [];
+      for (let j = 0; j < this.wordCount; j++) {
+        row.push({ color: '', letter: '' });
+      }
+      this.guesses.push(row);
+    }
+  }
+
+  row1 = 'QWERTYUIOP'.split('');
+  row2 = 'ASDFGHJKL'.split('');
+  row3 = 'ZXCVBNM'.split('');
+
+  addLetter(letter: string) {
+    if (this.guessColumn >= this.wordCount) {
+      return;
+    }
+
+    this.guesses[this.attemptIndex][this.guessColumn].color = '';
+
+    this.guesses[this.attemptIndex][this.guessColumn] = {
+      color: '',
+      letter: letter,
+    };
+
+    this.guessColumn++;
+    if (this.guessColumn < this.wordCount) {
+      this.guesses[this.attemptIndex][this.guessColumn].color = 'focusCell';
+    }
+  }
+
+  removeLetter() {
+    if (this.guessColumn < this.wordCount) {
+      this.guesses[this.attemptIndex][this.guessColumn].color = '';
+    }
+
+    this.guessColumn = Math.max(this.guessColumn - 1, 0);
+    this.guesses[this.attemptIndex][this.guessColumn] = {
+      color: 'focusCell',
+      letter: '',
+    };
+  }
+
+  enter() {
+    if (this.guessColumn < this.wordCount) {
+      this.errorMsg = 'Too short';
+      return;
+    }
+    this.errorMsg = '';
+    this.guesses[this.attemptIndex] = this.validateAnswer();
+  }
+
+  validateAnswer(): Cell[] {
+    const guessArr = this.guesses[this.attemptIndex]
+      .map((cell) => cell.letter)
+      .join('')
+      .toUpperCase()
+      .split('');
+
+    const solutionArr = this.wordSolution.toUpperCase().split('');
+    const solutionCopy = [...solutionArr];
+
+    const result: Cell[] = [];
+
+    let points = 0;
+
+    guessArr.forEach((letter, index) => {
+      if (letter === solutionArr[index]) {
+        result.push({ letter, color: 'correctLetter' });
+        points++;
+        solutionCopy[index] = null as any;
+      } else {
+        result.push({ letter, color: 'absentLetter' });
+      }
+    });
+
+    if (points === 5) {
+      this.errorMsg = 'Has ganado';
+    }
+
+    result.forEach((entry, index) => {
+      if (
+        entry.color === 'absentLetter' &&
+        solutionCopy.includes(entry.letter)
+      ) {
+        entry.color = 'presentLetter';
+        solutionCopy[solutionCopy.indexOf(entry.letter)] = null as any;
+      }
+    });
+    this.attemptIndex++;
+    this.guessColumn = 0;
+    return result;
+  }
 }
